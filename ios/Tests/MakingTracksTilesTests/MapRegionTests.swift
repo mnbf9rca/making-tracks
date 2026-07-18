db1856f8eaa978d29d3e88e5b498e3b69b2be658
Rob Aleck <mnbf9rca@users.noreply.github.com>
2026-07-18 11:00:56 +0100

    Add map update and empty-state surfaces (#199)

--- ios/Tests/MakingTracksTilesTests/MapRegionTests.swift
+++ ios/Tests/MakingTracksTilesTests/MapRegionTests.swift
@@ -12,4 +12,17 @@ final class MapRegionTests: XCTestCase {
         XCTAssertEqual(MapRegion.select(for: ocean, current: .uk), .uk)
         XCTAssertEqual(MapRegion.select(for: ocean, current: nil), .uk)
     }
+
+    func testSupportedRegionReportsCoverageWithoutChangingSelectionFallback() {
+        let malaysia = BBox(minLon: 101.64, minLat: 3.09, maxLon: 101.74, maxLat: 3.19)
+        let uk = BBox(minLon: -0.13, minLat: 51.48, maxLon: -0.11, maxLat: 51.50)
+        let ocean = BBox(minLon: -32.0, minLat: 22.0, maxLon: -20.0, maxLat: 28.0)
+        let wideSupported = BBox(minLon: -8.65, minLat: 0.85, maxLon: 119.27, maxLat: 60.86)
+
+        XCTAssertEqual(MapRegion.supportedRegion(for: malaysia), .malaysia)
+        XCTAssertEqual(MapRegion.supportedRegion(for: uk), .uk)
+        XCTAssertNotNil(MapRegion.supportedRegion(for: wideSupported))
+        XCTAssertNil(MapRegion.supportedRegion(for: ocean))
+        XCTAssertEqual(MapRegion.select(for: ocean, current: nil), .uk)
+    }
 }
